9adfbdcb152b01e9a8f573d2ba1b8d55b960b733
mk294753618 <122676598+mk294753618@users.noreply.github.com>
2024-06-28 08:47:33 +0800

    fixed-bug

--- main-page/color-setting.js
+++ main-page/color-setting.js
@@ -8,27 +8,33 @@ const redbtn = document.querySelector('.red');
 const bluebtn = document.querySelector('.blue');
 const yellowbtn = document.querySelector('.yellow');
 const blackbtn = document.querySelector('.black');
+const confirmBtn = document.querySelector('.confirm')
 
 redbtn.addEventListener('click', function() {
     updateSetting('color', 'red');
+    location.reload();
 });
 
 bluebtn.addEventListener('click', function() {
     updateSetting('color', 'blue');
+    location.reload();
 });
 
 yellowbtn.addEventListener('click', function() {
     updateSetting('color', 'yellow');
+    location.reload();
 });
 
 blackbtn.addEventListener('click', function() {
     updateSetting('color', 'black');
+    location.reload();
 });
 
 
-contrastRatioValue.addEventListener('change', function() {
+confirmBtn.addEventListener('click', function(){ 
     updateSetting('contrast', contrastRatioValue.value);
-});
+    location.reload();
+})
 
 
 function updateSetting(type, value) {
@@ -42,25 +48,33 @@ function updateSetting(type, value) {
 
 var profile = document.querySelector('.profile');
 var settings = document.querySelector('.setting');
-var page = document.querySelector('.page')
+var page = document.querySelector('.page');
 const colorChange = function(){
-    var num = 255 - setting.contrast;
-    page.style.backgroundColor = `rgb(${num},${num},${num})`
     if(setting.color == 'red'){
+        var num = 255 - setting.value;
+        page.style.backgroundColor = `rgb(${num},${num},${num})` 
         settings.style.borderColor = "#f00";
         profile.style.borderColor = "#f00";
+        confirmBtn.style.borderColor = "#f00";
+        confirmBtn.style.backgroundColor = "#f00";
     }
     if(setting.color == 'yellow'){
         settings.style.borderColor = "#ff0";
         profile.style.borderColor = "#ff0";
+        confirmBtn.style.borderColor = "#ff0";
+        confirmBtn.style.backgroundColor = "#ff0";
     }
     if(setting.color == 'blue'){
         settings.style.borderColor = "#00f";
         profile.style.borderColor = "#00f";
+        confirmBtn.style.borderColor = "#00f";
+        confirmBtn.style.backgroundColor = "#00f";
     }
     if(setting.color == 'black'){
         settings.style.borderColor = "#000";
         profile.style.borderColor = "#000";
+        confirmBtn.style.borderColor = "#000";
+        confirmBtn.style.backgroundColor = "#000";
     }
 }
 
